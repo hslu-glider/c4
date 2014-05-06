b3f5fdbab5818aba46e45834597ed1aade649319
Jack67 <michael.mueller.02@stud.hslu.ch>
2014-05-06 13:22:37 +0200

    Projekt KI angelegt

---
+++ C4_KI/src/c4_ki/C4_KI.java
@@ -0,0 +1,35 @@
+/*
+ *  C4 - Connect Four
+ *  Copyright (C) 2014 by Ervin Mazlagic, Jan Vonmoos, Alain Studhalter,
+ *  Michael Müller, Lukas Luthiger
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+package c4_ki;
+
+/**
+ *
+ * @author Michael Müller michael.mueller.02@stud.hslu.ch
+ * 
+ */
+public class C4_KI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
