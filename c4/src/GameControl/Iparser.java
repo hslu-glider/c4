7398084811447e89c3b9f9e5156e6922666bdfdd
Nino Ninux <nino.ninux@gmail.com>
2014-05-13 16:16:11 +0200

    Parser Interface erstellt

---
+++ c4/src/GameControl/Iparser.java
@@ -0,0 +1,29 @@
+
+
+package GameControl;
+
+import java.net.InetAddress;
+
+/**
+ *
+ * @author ninux
+ */
+public interface Iparser {
+	
+	/**
+	 * Look for a open TCP server.
+	 * @return 
+	 */
+	public InetAddress searchPlayer();
+	
+	/**
+	 * Waits for a broadcast from a client and sends back 
+	 * Inet Adress for connection.
+	 */
+	public void waitForPlayer();
+	
+	public boolean connectToPlayer(InetAddress adr);
+	
+	public int sendMove(int row);
+	
+}
